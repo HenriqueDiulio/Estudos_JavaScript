30c2acd491e56e1aba2d08d08ea96df68814e0e8
HenriqueDiulio <henriquediulio3@gmail.com>
2022-12-19 16:57:20 -0300

    Introdução a Java Script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 // declaracao 
 var nome = "Henrique";
 var  idade  = 29;
@@ -19,4 +20,39 @@ console.log(frase.replace("Japao","Brasil"));
 
 // to Upper case e lower case funcionam aqui. 
 console.log(frase.toUpperCase());
+*/
 
+// craindo uma lista
+var lista = ["maça", "Péra", "Laranja"];
+
+// listando o item de pocisão 1
+console.log(lista[1]);
+
+// adcioanando mais um item
+lista.push("uva");
+console.log(lista);
+
+// remorer item
+lista.pop("uva");
+console.log(lista);
+
+// ver o temanha da lista
+console.log(lista.length);
+
+//para imprimir, e mostra item de pocicao 1
+console.log(lista.toString()[1]);  
+
+// imprime os item e separar por espaços 
+console.log(lista.join("-"))
+
+
+/*
+// dicionario
+var fruta = {nome:"Uva", cor:"roxa"};
+console.log(fruta.nome);
+
+*/
+// lista de dicionario
+var frutas = [{nome:"Uva", cor:"roxa"}, {nome:"laranja", cor:"laranja"}];
+console.log(frutas);
+alert(frutas[1].nome);

e6584feb7ebabc867acf09e5c90d2dfa0614bfce
HenriqueDiulio <henriquediulio3@gmail.com>
2022-12-19 17:41:55 -0300

    Introdução Js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-/*
+
 // declaracao 
 var nome = "Henrique";
-var  idade  = 29;
+var idade = 18;
 var idade2 = 10;
 var frase = "Japao e o melhor time do mundo"
-
+/*
 // alert emite um alerta na sua tela;
 alert("Seja Bem vindo a minha página "+nome+"!");
 
 
-// ele tamb efetua concatenacao entre variaveis
+// efetua concatenacao entre variaveis
 alert(idade + idade2);
 
 console.log(nome);
@@ -20,7 +20,7 @@ console.log(frase.replace("Japao","Brasil"));
 
 // to Upper case e lower case funcionam aqui. 
 console.log(frase.toUpperCase());
-*/
+
 
 // craindo uma lista
 var lista = ["maça", "Péra", "Laranja"];
@@ -43,16 +43,44 @@ console.log(lista.length);
 console.log(lista.toString()[1]);  
 
 // imprime os item e separar por espaços 
-console.log(lista.join("-"))
+console.log(lista.join("-"));s
 
-
-/*
 // dicionario
 var fruta = {nome:"Uva", cor:"roxa"};
 console.log(fruta.nome);
 
-*/
 // lista de dicionario
 var frutas = [{nome:"Uva", cor:"roxa"}, {nome:"laranja", cor:"laranja"}];
 console.log(frutas);
 alert(frutas[1].nome);
+
+// Recebendo valores e armazenado
+var idade3 = prompt("Qual sua idade: ");
+var nome;
+
+// estrutura condicional
+if (idade >= 18){
+    alert("Maior de idade");
+};
+    else{
+    alert("Menor de idade");
+};
+
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    count++;
+};
+
+
+var count2;
+ 
+for (count2 = 0, count2 <=5, count2++){   
+    console.log(count2); 
+};
+
+*/
+
+var d  = new Date();
+alert(d.getDate());
+alert(d.getHours());  

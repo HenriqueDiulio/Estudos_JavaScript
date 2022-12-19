287797c65d4c7ce455b08f60126b9147c36d5210
HenriqueDiulio <henriquediulio3@gmail.com>
2022-12-19 09:51:41 -0300

    introducao JavaScript

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// declaracao 
+var nome = "Henrique";
+var  idade  = 29;
+var idade2 = 10;
+var frase = "Japao e o melhor time do mundo"
+
+// alert emite um alerta na sua tela;
+alert("Seja Bem vindo a minha página "+nome+"!");
+
+
+// ele tamb efetua concatenacao entre variaveis
+alert(idade + idade2);
+
+console.log(nome);
+console.log(idade);
+
+// replace fazer alteracoes na string.
+console.log(frase.replace("Japao","Brasil"));
+
+// to Upper case e lower case funcionam aqui. 
+console.log(frase.toUpperCase());
+
